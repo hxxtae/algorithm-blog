de87224f6b13f08b6e00d18dc1f052ae68d92d4a
hxxtae <fkdlxmfkdl@gmail.com>
2023-12-16 17:58:55 +0900

    :books: [Docs]: Update header logo
    - apply theme

--- components/header/page.tsx
+++ components/header/page.tsx
@@ -1,10 +1,12 @@
 'use client';
+import { useEffect, useState } from 'react';
+import { useTheme } from 'next-themes';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from './page.module.css';
-import { useEffect } from 'react';
-import { useTheme } from 'next-themes';
 
 export default function Header() {
 
@@ -48,15 +50,8 @@ export default function Header() {
   return (
     <div className={styles.header} id='header'>
       <div className={styles.header_left}>
-        <Link href='/'>
-          <Image
-            className={styles.header_logo}
-            src="/assets/header_logo.png"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
+        <Link className={styles.header_logo} href='/'>
+          <ThemeLogo />
         </Link>
         <Link href={'/'}>Home</Link>
         <Link href={'/boj'}>BOJ</Link>
@@ -71,15 +66,66 @@ export default function Header() {
 
 function ThemeChanger() {
   const { setTheme, theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   const toggleTheme = () => {
     setTheme(theme === "dark" ? "light" : "dark");
   };
 
+  // For hydration
+  if (!mounted) return;
+
   return (
-    <button type='button' onClick={toggleTheme}>
-      <strong className="dark:hidden">Light</strong>
-      <strong className="dark:block">Dark</strong>
+    <button className={styles.theme_button} type='button' onClick={toggleTheme}>
+      <span data-theme={theme !== 'dark' ? 'show' : 'hidden'}><FontAwesomeIcon icon={faSun} size='1x' /></span>
+      <span data-theme={theme === 'dark' ? 'show' : 'hidden'}><FontAwesomeIcon  icon={faMoon} size='1x' /></span>
     </button>
+  )
+}
+
+// ⚠ Avoid Hydration Mismatch
+// 서버(SSI)의 테마를 알 수 없기 때문에 useTheme에서 반환된 많은 값은 클라이언트에 마운트될(CSR) 때까지 정의되지 않습니다.
+// 즉, 클라이언트에 탑재하기 전에 현재 테마를 기반으로 UI를 렌더링하려고 하면 수화(Hydration) 불일치 오류가 표시됩니다.
+// ex) 기본 테마가 light인 경우 dark 모드에서 새로고침(서버 재접속)시 서버와 클라이언트의 theme가 불일치
+
+// ✨ fix hydration of theme
+// 이 문제를 해결하려면 페이지가 클라이언트(CSR)에 탑재될 때 현재 테마를 사용하는 UI만 렌더링해야 합니다.
+
+function ThemeLogo() {
+  const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  
+  // For hydration
+  if (!mounted) return;
+
+  return (
+    <>
+      {theme === 'dark' ? (
+        <Image
+          className={styles.header_logo}
+          src="/assets/header_logo_light.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      ) :
+        <Image
+          className={styles.header_logo}
+          src="/assets/header_logo_dark.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />}
+    </>
   )
 }
